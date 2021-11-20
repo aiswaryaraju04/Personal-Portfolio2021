c0a33f0a689dbbc7d4267722b116ee74df09e9e5
Aiswarya Raju <aiswaryaraju2001@gmail.com>
2021-11-20 21:55:38 +0530

    New File

---
+++ js/type.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.animate', {
+    strings: [
+        "Aiswarya Raju",
+        "A Developer",
+        "A Designer",
+        "An Entrepreneur"
+],
+    typeSpeed: 50,
+    backSpeed: 60,
+    loop: true
+});
